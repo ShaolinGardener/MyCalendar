2e5cb22c2744dbf28d1147f634984d2f2346b8d7
Chaz Stone <mmcxiit4e@gmail.com>
2018-10-13 16:51:50 -0400

    last change for now

--- MyCalendar/Controllers/CalendarController.cs
+++ MyCalendar/Controllers/CalendarController.cs
@@ -12,82 +12,10 @@ namespace MyCalendar.Controllers
         // GET: Calendar
         public ActionResult Index()
         {
-            return View();
+            return View("CalendarContainer");
         }
 
-        // GET: Calendar/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Calendar/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Calendar/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Calendar/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Calendar/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Calendar/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Calendar/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        [HttpGet]
+         [HttpGet]
         public ActionResult Month(string month, string year)
         {
             if(month==null || year == null)
@@ -132,9 +60,9 @@ namespace MyCalendar.Controllers
                     Instructor = null,
                 }
             };
-
             return View(events);
         }
+
         [HttpGet]
         public ActionResult CalendarContainer(string month, string year)
         {
@@ -147,18 +75,83 @@ namespace MyCalendar.Controllers
             ViewBag.Month = int.Parse(month);
             ViewBag.Year = int.Parse(year);
 
-            //List<Event> events = new List<Event>
-            //{
-            //    new Event()
-            //    {
-            //        Name = "First One",
-            //        Start = DateTime.Parse("10/11/2018 09:00"),
-            //        End = DateTime.Parse("10/11/2018 10:00"),
-            //        Class = null,
-            //        Instructor = null,
-            //    }
-            //};
             return View();
         }
     }
 }
+
+#region Not needed for now
+
+//// GET: Calendar/Details/5
+//public ActionResult Details(int id)
+//{
+//    return View();
+//}
+
+//// GET: Calendar/Create
+//public ActionResult Create()
+//{
+//    return View();
+//}
+
+//// POST: Calendar/Create
+//[HttpPost]
+//public ActionResult Create(FormCollection collection)
+//{
+//    try
+//    {
+//        // TODO: Add insert logic here
+
+//        return RedirectToAction("Index");
+//    }
+//    catch
+//    {
+//        return View();
+//    }
+//}
+
+//// GET: Calendar/Edit/5
+//public ActionResult Edit(int id)
+//{
+//    return View();
+//}
+
+//// POST: Calendar/Edit/5
+//[HttpPost]
+//public ActionResult Edit(int id, FormCollection collection)
+//{
+//    try
+//    {
+//        // TODO: Add update logic here
+
+//        return RedirectToAction("Index");
+//    }
+//    catch
+//    {
+//        return View();
+//    }
+//}
+
+//// GET: Calendar/Delete/5
+//public ActionResult Delete(int id)
+//{
+//    return View();
+//}
+
+//// POST: Calendar/Delete/5
+//[HttpPost]
+//public ActionResult Delete(int id, FormCollection collection)
+//{
+//    try
+//    {
+//        // TODO: Add delete logic here
+
+//        return RedirectToAction("Index");
+//    }
+//    catch
+//    {
+//        return View();
+//    }
+//}
+
+#endregion
